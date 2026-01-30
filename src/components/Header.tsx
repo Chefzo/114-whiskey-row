ec3144d564a24e8e7c63436a26b76dc69182078c
Wix Remote Machine <support@wix.com>
2026-01-30 02:03:32 +0000

    Update button text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     { path: '/gallery', label: 'Gallery' },
     { path: '/visit', label: 'Visit' },
     { path: '/about', label: 'About' },
-    { path: '/menu', label: 'Menu' },
+    { path: '/menu', label: 'DonFathers Menu' },
     { path: '/contact', label: 'Contact' },
   ];
 
